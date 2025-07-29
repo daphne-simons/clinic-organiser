0bee9ff23f0173f782030b6c022ed1eae3a36fbb
Daphne Simons <daphnejasminesimons@gmail.com>
2025-07-29 15:22:41 +1200

    get client by id api works

--- src/apis/clients.ts
+++ src/apis/clients.ts
@@ -2,29 +2,17 @@ import request from 'superagent'
 import type { IClient, IClientForDropdown } from '../models';
 
 export async function getAllClientNames() {
-  // TODO: GET from db
-
   const data = await request.get('http://localhost:3000/api/v1/clients/')
-
   return data.body.map((client: IClient) => {
     return {
       id: client.id,
       label: `${client.first_name} ${client.last_name}`,
     }
   }) as IClientForDropdown[]
-
-  //   [
-  //     {
-  //       id: 1,
-  //       label: "Jared Pinfold",
-  //     },
-  //     { id: 2, label: "Daph Simons" },
-  //     { id: 3, label: "Destroy Orbison" },
-  //   ];
 }
-export function getClientById(id: number) {
-  // TODO: GET from db
-  console.log(id);
+export async function getClientById(id: number) {
+  const data = await request.get(`http://localhost:3000/api/v1/clients/${id}`)
+  return data.body as IClient
 }
 
 export function addClient(client: unknown) {
--- src/components/ClientsTab.tsx
+++ src/components/ClientsTab.tsx
@@ -1,11 +1,20 @@
+import { useQuery } from "@tanstack/react-query"
 import type { View } from "./Layout"
+import { getClientById } from "../apis/clients"
 
 interface IProps {
   view: View
 }
 
-export default function ClientsTab({view}: IProps) {
-  const {subTab} = view.props
+export default function ClientsTab({ view }: IProps) {
+  const { subTab } = view.props
+
+  // TODO: think about where these client queries should live: 
+  // const { data, isLoading, isError } = useQuery({ queryKey: ["clients", "uniqueId"], queryFn: () => getClientById(1) })
+  // if (isLoading) return <div>Loading...</div>
+  // if (isError) return <div>Error</div>
+  // console.log(data);
+
   return (
     <div>
       <h1>Clients</h1>
