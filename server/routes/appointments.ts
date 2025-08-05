cc1dde058786be53f6259a833401ddfd2add3d99
Daphne Simons <daphnejasminesimons@gmail.com>
2025-08-05 13:26:56 +1200

    appointments has POST, PATCH and DEL routes in BE

--- server/routes/appointments.ts
+++ server/routes/appointments.ts
@@ -2,14 +2,11 @@ import express from 'express'
 import pool from '../database/config/connection'
 const router = express.Router()
 
-// GET `api/v1/appointments`
+// GET `api/v1/appointments/`
 router.get('/', async (req, res) => {
   try {
     // NOTE: postgreSQL automatically lowercases aliases, so to retain camelCase, double quotes are used. 
-    const result = await pool.query('SELECT client_id AS "clientId", start_time AS "startTime", end_time AS "endTime", appointment_type AS "appointmentType", notes FROM appointments')
-
-    console.log(result.rows);
-
+    const result = await pool.query('SELECT id, client_id AS "clientId", start_time AS "startTime", end_time AS "endTime", appointment_type AS "appointmentType", notes FROM appointments')
     res.status(200).json(result.rows)
   } catch (err) {
     console.error(err);
@@ -17,10 +14,11 @@ router.get('/', async (req, res) => {
   }
 })
 
+// POST `api/v1/appointments/`
 router.post('/', async (req, res) => {
-  const { title, color } = req.body
+  const { clientId, startTime, endTime, appointmentType, notes } = req.body
   try {
-    await pool.query('INSERT INTO categories (title, color) VALUES ($1, $2)', [title, color])
+    await pool.query('INSERT INTO appointments (client_id, start_time, end_time, appointment_type, notes) VALUES ($1, $2, $3, $4, $5)', [clientId, startTime, endTime, appointmentType, notes])
     res.sendStatus(201)
   } catch (err) {
     console.error(err);
@@ -28,10 +26,24 @@ router.post('/', async (req, res) => {
   }
 })
 
+// PATCH `api/v1/appointments/4` 
+router.patch('/:id', async (req, res) => {
+  const { id } = req.params
+  const { clientId, startTime, endTime, appointmentType, notes } = req.body
+  try {
+    await pool.query('UPDATE appointments SET client_id = $1, start_time = $2, end_time = $3, appointment_type = $4, notes = $5 WHERE id = $6', [clientId, startTime, endTime, appointmentType, notes, id])
+    res.sendStatus(204)
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal Server Error' })
+  }
+})
+
+// DELETE `api/v1/appointments/4`
 router.delete('/:id', async (req, res) => {
   const { id } = req.params
   try {
-    await pool.query('DELETE FROM categories WHERE id = ($1)', [id])
+    await pool.query('DELETE FROM appointments WHERE id = ($1)', [id])
     res.sendStatus(204)
   } catch (err) {
     console.error(err);
