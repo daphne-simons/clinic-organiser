9a033899625c140019a29f19862905e4fe67ade4
Daphne Simons <daphnejasminesimons@gmail.com>
2025-08-12 19:34:40 +1200

    remove unused code

--- src/components/AppointmentInfo.tsx
+++ src/components/AppointmentInfo.tsx
@@ -2,9 +2,7 @@ import { Typography } from "@mui/material";
 import type { IAppointmentInfo } from "../models";
 import type { EventProps } from "react-big-calendar";
 
-// interface IProps extends EventProps<IAppointmentInfo> {
-//   appointment: IAppointmentInfo
-// }
+/////////////////////////////////////////////////////////////////////////////
 
 const AppointmentInfo = ({ event }: EventProps<IAppointmentInfo>) => {
 
