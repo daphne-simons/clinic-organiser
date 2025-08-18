8b8043cc874840ad0cc1cc4f8e23cf8240696291
Jared Pinfold <j.pinfold@gmail.com>
2025-08-18 17:01:32 +1200

    add tests for categories db functions

---
+++ tests/db-tests/categories.test.ts
@@ -0,0 +1,59 @@
+import { afterAll, beforeAll, beforeEach, describe, expect, it } from "vitest"
+
+import {
+  addCategory,
+  getAllCategories,
+  deleteCategory,
+} from "../../server/database/dbFunctions/categories"
+import { resetTestDatabase, reseedTestDatabase } from "../test-utils"
+
+beforeAll(async () => {
+  await resetTestDatabase()
+})
+beforeEach(async () => {
+  await reseedTestDatabase()
+})
+afterAll(async () => {})
+
+describe("getAllCategories", async () => {
+  it("returns all categories", async () => {
+    const expected = [
+      { _id: 1, color: "blue", title: "ACC" },
+      { _id: 2, color: "green", title: "Private" },
+    ]
+
+    const result = await getAllCategories()
+    expect(result).toEqual(expected)
+  })
+})
+
+describe("addCategory", async () => {
+  it("creates an entry in the categories table when provided with a title and color", async () => {
+    const expected = [
+      { _id: 1, color: "blue", title: "ACC" },
+      { _id: 2, color: "green", title: "Private" },
+      { _id: 3, color: "red", title: "Free Trial" },
+    ]
+    await addCategory("Free Trial", "red")
+    const result = await getAllCategories()
+    expect(result).toEqual(expected)
+  })
+})
+
+describe("deleteCategory", async () => {
+  it("deletes an entry in the categories table when provided with an id", async () => {
+    const expected = [{ _id: 1, color: "blue", title: "ACC" }]
+    await deleteCategory(2)
+    const result = await getAllCategories()
+    expect(result).toEqual(expected)
+  })
+  it("fails if id is not in number format", async () => {
+    expect.assertions(1) // set the number of expected assertions - in this case the test can only pass if the catch block is triggered
+    try {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      await deleteCategory("bad id" as any)
+    } catch (error) {
+          expect(error).toBeTruthy()
+    }
+  })
+})
