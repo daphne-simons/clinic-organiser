499fb5cc64b78cf802b5db62716fec3f175c6707
Daphne Simons <daphnejasminesimons@gmail.com>
2025-08-19 14:20:44 +1200

    Basic Auth0 setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,25 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 // import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import "./index.css";
+import { Auth0Provider } from '@auth0/auth0-react';
 import App from "./App.tsx";
-
+import "./index.css";
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      {/* <ReactQueryDevtools /> */}
-      <App />
-    </QueryClientProvider>
-  </StrictMode>
+    <Auth0Provider
+      domain={import.meta.env.VITE_AUTH0_DOMAIN}
+      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+        audience: import.meta.env.VITE_AUTH0_AUDIENCE
+      }}>
+      <QueryClientProvider client={queryClient}>
+        {/* <ReactQueryDevtools /> */}
+        <App />
+      </QueryClientProvider>
+    </Auth0Provider>
+  </StrictMode >
 );
