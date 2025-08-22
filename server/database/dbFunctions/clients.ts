70a5eb2ec9f3456e88fdda521822147824a9b128
Daphne Simons <daphnejasminesimons@gmail.com>
2025-08-22 12:26:41 +1200

    create be route and database function for adding client from appointments form and clients form.

--- server/database/dbFunctions/clients.ts
+++ server/database/dbFunctions/clients.ts
@@ -1,5 +1,5 @@
 import pool from "../config/connection.ts"
-import { IClientName, IClient } from "../../../src/models.ts"
+import { IClientName, IClient, CustomFields } from "../../../src/models.ts"
 
 export async function getAllClientsNames(): Promise<IClientName[]> {
   const result = await pool.query(
@@ -11,4 +11,17 @@ export async function getAllClientsNames(): Promise<IClientName[]> {
 export async function getClientById(id: string): Promise<IClient> {
   const result = await pool.query("SELECT * FROM clients WHERE id = $1", [id])
   return result.rows[0]
+}
+
+export async function addClientFromAppointment(firstName: string, lastName: string): Promise<void> {
+  await pool.query(
+    "INSERT INTO clients (first_name, last_name) VALUES ($1, $2)",
+    [firstName, lastName]
+  )
+}
+
+export async function addClient(firstName: string, lastName: string, dob: string, mobile: string, email: string, customFields: CustomFields): Promise<void> {
+  await pool.query(
+    "INSERT INTO clients (first_name, last_name, dob, mobile, email, custom_fields) VALUES ($1, $2, $3, $4, $5, $6)",
+    [firstName, lastName, dob, mobile, email, customFields])
 }
--- server/routes/clients.ts
+++ server/routes/clients.ts
@@ -3,6 +3,8 @@ const router = express.Router()
 import checkJwt from "../auth"
 
 import {
+  addClient,
+  addClientFromAppointment,
   getAllClientsNames,
   getClientById,
 } from "../database/dbFunctions/clients"
@@ -22,6 +24,31 @@ router.get("/:id", checkJwt, async (req, res) => {
     const { id } = req.params
     const result = await getClientById(id)
     res.status(200).json(result)
+  } catch (err) {
+    console.error(err)
+    res.status(500).json({ error: "Internal Server Error" })
+  }
+})
+
+router.post("/", checkJwt, async (req, res) => {
+  const { formSource } = req.body
+  try {
+    if (formSource === "appointments") {
+      const { firstName, lastName } = req.body
+      await addClientFromAppointment(firstName, lastName)
+    }
+    if (formSource === "clients") {
+      const {
+        firstName,
+        lastName,
+        dob,
+        mobile,
+        email,
+        customFields } = req.body
+      await addClient(firstName, lastName, dob, mobile, email, customFields)
+    }
+
+    res.sendStatus(201)
   } catch (err) {
     console.error(err)
     res.status(500).json({ error: "Internal Server Error" })
